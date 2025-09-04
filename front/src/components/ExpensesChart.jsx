466a268a200438201565d6b2df3a117a1694d570
natolotra <natolotra@gmail.com>
2025-09-04 15:53:08 +0200

    feat: ajout du composant ExpensesChart avec styles avec fonctionnalité

--- front/src/components/ExpensesChart.jsx
+++ front/src/components/ExpensesChart.jsx
@@ -1,4 +1,5 @@
-import React from 'react';$
+import React from 'react';
+import "./ExpensesChart.css";
 import { useState, useEffect } from 'react';
 import { Bar } from "react-chartjs-2";
 import {
@@ -13,12 +14,22 @@ import {
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const ExpensesChart = () => {
+        useEffect(() => {
+            const handleResize = () => {
+                window.dispatchEvent(new Event('resize'));
+            };
+
+                window.addEventListener('resize', handleResize);
+                return () => window.removeEventListener('resize', handleResize);
+        }, []);
+
     const data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+          "November", "December"],
       datasets: [
         {
           label: "Expenses",
-          data: [65, 59, 80, 81, 56, 55, 40],
+          data: [65, 59, 80, 81, 56, 55, 40, 90,100,10,30,70],
           backgroundColor: [
             "rgba(255, 99, 132, 0.5)",
             "rgba(255, 159, 64, 0.5)",
@@ -26,6 +37,11 @@ export const ExpensesChart = () => {
             "rgba(75, 192, 192, 0.5)",
             "rgba(54, 162, 235, 0.5)",
             "rgba(153, 102, 255, 0.5)",
+            "rgba(201, 203, 207, 0.5)",
+            "rgba(201, 203, 207, 0.5)",
+            "rgba(201, 203, 207, 0.5)",
+            "rgba(201, 203, 207, 0.5)",
+            "rgba(201, 203, 207, 0.5)",
             "rgba(201, 203, 207, 0.5)",
           ],
           borderColor: [
@@ -36,13 +52,18 @@ export const ExpensesChart = () => {
             "rgb(54, 162, 235)",
             "rgb(153, 102, 255)",
             "rgb(100, 100, 100)",
+            "rgb(100, 100, 100)",
+            "rgb(100, 100, 100)",
+            "rgb(100, 100, 100)",
+            "rgb(100, 100, 100)",
+            "rgb(100, 100, 100)",
           ],
           borderWidth: 1,
         },
         {
           label: "Salary",
-          data: [100, 120, 110, 130, 90, 95, 85], // Exemple de valeurs de salaire
-          backgroundColor: "rgba(54, 162, 235, 0.5)", // Bleu pour le salaire
+          data: [100, 120, 110, 130, 90, 95, 85, 10, 20, 50, 500, 100],
+          backgroundColor: "rgba(54, 162, 235, 0.5)",
           borderColor: "rgb(54, 162, 235)",
           borderWidth: 1,
         },
@@ -62,18 +83,11 @@ export const ExpensesChart = () => {
       },
     };
 
-    useEffect(() => {
-        const handleResize = () => {
-            window.dispatchEvent(new Event('resize'));
-        };
 
-            window.addEventListener('resize', handleResize);
-            return () => window.removeEventListener('resize', handleResize);
-    }, []);
 
      return (
         <>
-        <div className="chart-container">
+        <div className="bar-chart-container">
             <Bar data={data} options={options} />
         </div>
         </>
