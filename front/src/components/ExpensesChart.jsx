16b77b0d0016d0abc64f3a50deeffe13b30e8453
natolotra <natolotra@gmail.com>
2025-09-09 15:17:38 +0200

    fix: modification du barChart qui s'adapte au dashboard

--- front/src/components/ExpensesChart.jsx
+++ front/src/components/ExpensesChart.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import "./ExpensesChart.css";
-import { useState, useEffect } from 'react';
 import { Bar } from "react-chartjs-2";
 import {
     Chart as ChartJS,
@@ -11,18 +9,11 @@ import {
     Tooltip,
     Legend,
 } from "chart.js";
+import './ExpensesChart.css'
+
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 export const ExpensesChart = () => {
-        useEffect(() => {
-            const handleResize = () => {
-                window.dispatchEvent(new Event('resize'));
-            };
-
-                window.addEventListener('resize', handleResize);
-                return () => window.removeEventListener('resize', handleResize);
-        }, []);
-
     const data = {
       labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
           "November", "December"],
@@ -31,40 +22,40 @@ export const ExpensesChart = () => {
           label: "Expenses",
           data: [65, 59, 80, 81, 56, 55, 40, 90,100,10,30,70],
           backgroundColor: [
-            "rgba(255, 99, 132, 0.5)",
-            "rgba(255, 159, 64, 0.5)",
-            "rgba(255, 205, 86, 0.5)",
-            "rgba(75, 192, 192, 0.5)",
-            "rgba(54, 162, 235, 0.5)",
-            "rgba(153, 102, 255, 0.5)",
-            "rgba(201, 203, 207, 0.5)",
-            "rgba(201, 203, 207, 0.5)",
-            "rgba(201, 203, 207, 0.5)",
-            "rgba(201, 203, 207, 0.5)",
-            "rgba(201, 203, 207, 0.5)",
-            "rgba(201, 203, 207, 0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
           ],
           borderColor: [
-            "rgb(255, 99, 132)",
-            "rgb(255, 159, 64)",
-            "rgb(255, 205, 86)",
-            "rgb(75, 192, 192)",
-            "rgb(54, 162, 235)",
-            "rgb(153, 102, 255)",
-            "rgb(100, 100, 100)",
-            "rgb(100, 100, 100)",
-            "rgb(100, 100, 100)",
-            "rgb(100, 100, 100)",
-            "rgb(100, 100, 100)",
-            "rgb(100, 100, 100)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
+            "rgba(51,251,7,0.5)",
           ],
           borderWidth: 1,
         },
         {
           label: "Salary",
           data: [100, 120, 110, 130, 90, 95, 85, 10, 20, 50, 500, 100],
-          backgroundColor: "rgba(54, 162, 235, 0.5)",
-          borderColor: "rgb(54, 162, 235)",
+          backgroundColor: "rgba(2,29,188,0.94)",
+          borderColor: "rgba(2,29,188,0.94)",
           borderWidth: 1,
         },
       ],
@@ -72,6 +63,7 @@ export const ExpensesChart = () => {
 
     const options = {
       responsive: true,
+        maintainAspectRatio: false,
       plugins: {
         legend: {
           position: "top",
@@ -93,5 +85,3 @@ export const ExpensesChart = () => {
         </>
       );
 }
-
-export default ExpensesChart;
