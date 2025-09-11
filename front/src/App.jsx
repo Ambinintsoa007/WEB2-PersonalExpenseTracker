8b3811025fd05173c77e3b5e25405a65aa88d804
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2025-09-11 16:23:34 +0300

    deleting useless styles in css

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,3 @@
-import { useState } from 'react'
-import './App.css'
-import {LoginPage} from "./components/LoginPage.jsx";
-import {SignUpPage} from "./components/SignUpPage.jsx";
-
-
 function App() {
   return (
       <>
