b74e4e21454b8885d9482712f0d1238b51fa2713
Njaka <hei.herinjaka@gmail.com>
2025-09-12 15:12:50 +0300

    feat: modification index et server

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,9 +1,15 @@
-import pkg from "pg";
-import dotenv from "dotenv";
+import pkg from 'pg';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
+
+pool.connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch(err => console.error("Database connection error:", err));
+
+export default pool;
